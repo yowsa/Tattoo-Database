be7d204befd94862c9c72d07cd2679089be73d1f
yowsa <josefin@fundin.com>
2019-09-06 13:21:13 +0100

    connected Flask to mySQL database

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+
 app = Flask(__name__)
 
-@app.route("/")
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'mynewdb'
+
+
+mysql = MySQL(app)
+
+
+
+@app.route("/hello")
 def hello():
 	return "Hello World!"
+
+
+
+@app.route('/')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT user, host FROM mysql.user''')
+    rv = cur.fetchall()
+    return str(rv)
+
+
+@app.route('/hi', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
