d493edd9ce1984a7c9fed5175d656c1e8e3a7b1e
yowsa <josefin@fundin.com>
2019-09-07 16:53:10 +0100

    added remove button that now removes any matching values from the form in the database

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 
@@ -13,24 +13,31 @@ app.config['MYSQL_DB'] = 'mynewdb'
 mysql = MySQL(app)
 
 
-
-@app.route("/hello")
-def hello():
-	return "Hello World!"
-
-
-
-@app.route('/')
+@app.route('/hey')
 def users():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT user, host FROM mysql.user''')
+    cur.execute('''SELECT * FROM MyUsers''')
     rv = cur.fetchall()
     return str(rv)
 
 
-@app.route('/hi', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        action = details['action']
+        cur = mysql.connection.cursor()
+        if action == "submit":
+        	cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        if action == "remove":
+            cur.execute("DELETE FROM MyUsers WHERE firstName=%s", (firstName,))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
     return render_template('index.html')
+
 
 
 if __name__ == '__main__':
--- main_tests.py
+++ main_tests.py
@@ -1,1 +1,6 @@
 main_tests.py
+
+import unittest 
+
+setup skapa ny databas, tabell etc
+teardown  ta bort databas etc
