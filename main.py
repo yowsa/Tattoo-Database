6d7373ff8f17d7d3f7e5b9e5cf31bd3d1ca7c0ec
yowsa <josefin@fundin.com>
2019-09-08 13:19:05 +0100

    change from flask-mysqldb to pymysql so it can be compatible with python3

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+import pymysql.cursors
 
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'mynewdb'
-
-
-mysql = MySQL(app)
-
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='password',
+                             db='mynewdb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 @app.route('/hey')
 def users():
-    cur = mysql.connection.cursor()
+    cur = connection.cursor()
     cur.execute('''SELECT * FROM MyUsers''')
     rv = cur.fetchall()
     return str(rv)
@@ -28,12 +26,13 @@ def index():
         firstName = details['fname']
         lastName = details['lname']
         action = details['action']
-        cur = mysql.connection.cursor()
+
+        cur = connection.cursor()
         if action == "submit":
         	cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
         if action == "remove":
             cur.execute("DELETE FROM MyUsers WHERE firstName=%s", (firstName,))
-        mysql.connection.commit()
+        connection.commit()
         cur.close()
         return 'success'
     return render_template('index.html')
