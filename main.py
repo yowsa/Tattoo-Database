dd86a33a195ec3231d4e8ba460cbe7dfb2aec539
yowsa <josefin@fundin.com>
2019-09-18 12:50:53 +0100

    added if statement in constructor hto help later with testting. Also added upload file funtion in the main.py file

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
+import os
 from flask import Flask, render_template, request
 import pymysql.cursors
 from table_manipulation import TattooManager
-
+from database import database_manager
 
 app = Flask(__name__)
 
 tattoo_manager = TattooManager()
 
+UPLOAD_FOLDER = os.getcwd() + '/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='password',
-                             db='mynewdb',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-connection.autocommit(True)
 
 @app.route('/hey')
 def users():
-    cur = connection.cursor()
-    cur.execute('''SELECT * FROM MyUsers''')
+    cur = database_manager.connection.cursor()
+    cur.execute('''SELECT * FROM tattoos''')
     rv = cur.fetchall()
-    print(rv)
     return str(rv)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    tattoo_manager.get_next_tattoo_id("tattoos", "tattoo_db")
     if request.method == "POST":
         details = request.form
-        firstName = details['fname']
-        lastName = details['lname']
+        file = request.files['vector_file']
+        tags = details['tag']
         action = details['action']
         if action == "submit":
-        	tattoo_manager.add_tattoo("MyUsers", firstName, lastName)
-        if action == "remove":
-        	tattoo_manager.remove_tattoo("MyUsers", firstName)
+            #tattoo_manager.add_tattoo(img_path)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            #print(tattoo_manager.get_tattoo_id())
+ #       if action == "remove":
+  #          tattoo_manager.remove_tattoo(tattoo_name)
         return 'success'
     return render_template('index.html')
 
--- table_manipulation.py
+++ table_manipulation.py
@@ -1,31 +1,32 @@
-from database import database_manager
 
 
 class TattooManager():
 
-	def add_tattoo(self, name, img_path):
-		database_manager.execute("INSERT INTO tattoos (name, img_path) VALUES (%s, %s)", (name, img_path))
+	def __init__(self, database_manager=None):
+		if database_manager is None:
+			from database import database_manager
+		self.database_manager = database_manager
+
+	def add_tattoo(self, img_path):
+		self.database_manager.execute("INSERT INTO tattoos (img_path) VALUES (%s)", (img_path))
 
 	def remove_tattoo(self, name):
-		database_manager.execute("DELETE FROM tattos WHERE name=%s", (name,))
+		self.database_manager.execute("DELETE FROM tattos WHERE name=%s", (name,))
 
-	def get_tattoo_id(self):
-		database_manager.execute("SELECT LAST_INSERT_ID() FROM tattoos")
-
-
-
-
-
-
-#def execute(a,b, *args, g=1, **kwargs):
-#	cur.execute(*, **kwarg)
+	def get_next_tattoo_id(self, table, database):
+		cur = self.database_manager.connection.cursor()
+		cur.execute("SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE table_name = %s AND table_schema=%s", (table, database))
+		rv = cur.fetchall()
+		cur.connection.commit()
+		cur.close()
+		print(rv)
 
 
 class TagManager():
 
-	def add_tag(self):
-		database_manager.execute("INSERT INTO tags (tag, tattoo_id) VALUES (%s, %s)", (tag, tattoo_id))
+	def add_tag(self, tag, tattoo_id):
+		self.database_manager.execute("INSERT INTO tags (tag, tattoo_id) VALUES (%s, %s)", (tag, tattoo_id))
 
 
-	def remove_tag(self):
-		database_manager.execute("DELETE FROM tags WHERE name=%s", (name,))
+	def remove_tag(self, name):
+		self.database_manager.execute("DELETE FROM tags WHERE name=%s", (name,))
