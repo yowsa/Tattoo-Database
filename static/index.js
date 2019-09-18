5c9bb19a3ae4f7915793517da881ab2e7a3bfa87
yowsa <josefin@fundin.com>
2019-09-18 12:52:12 +0100

    updated sql file for table setup, slight chnages to index to add img upload and remove certain fields

--- static/index.js
+++ static/index.js
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
 $.ajax({
 	type:"GET",
 	dataType: "json",
