9cdee0e4cfd8558615da30bfaaabdd236b9019ec
Josefin Fundin <josefin@fundin.com>
2020-01-17 14:38:17 +0000

    updated unique tags function to include count for each tag, set up flask apis in main as a start to deliver data to the FE

--- main.py
+++ main.py
@@ -1,10 +1,59 @@
 from database_connector import DatabaseConnector
 from database_manager import ItemManager, TagManager
+from search_manager import SearchManager
 
 database_connector = DatabaseConnector('db')
 connection = database_connector.get_connection()
 item_manager = ItemManager(database_connector)
 tag_manager = TagManager(database_connector)
+search_manager = SearchManager(tag_manager, item_manager)
 
+<<<<<<< HEAD
 item_id = item_manager.add_item()
 tag_manager.add_tag("bird", item_id)
+=======
+# item_id = item_manager.add_item()
+# tag_manager.add_tag("fineline", "00c33513-0248-433e-ba9c-91b1ba7a4f12")
+# print(tag_manager.get_all_matches("bird"))
+# print(item_manager.get_item("0064c184-b226-4a39-8259-343794104020"))
+# print(search_manager.get_all_maching_products("sw"))
+# print(tag_manager.get_item_tags("9d26bffc-0f7e-464f-874d-ce721bd1f015"))
+# print(tag_manager.get_unique_tags_list())
+
+
+from flask import Flask, jsonify
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, World, this is the index!'
+
+
+@app.route('/unique_tags', methods=['GET', 'POST'])
+def unique_tags():
+    if request.method == 'GET':
+        return jsonify(tag_manager.get_unique_tags_list())
+    else:
+        return "no tags requested"
+
+
+@app.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        return 'A product has been added'
+    else:
+        return "no button clicked yet to add a product"
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        return 'Word has been search and heres the result'
+    else:
+        return "no word submitted yet"
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+>>>>>>> updated unique tags function to include count for each tag, set up flask apis in main as a start to deliver data to the FE
