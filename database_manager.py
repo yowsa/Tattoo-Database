92f6566541bbe3b4a1fca41e88f0dd5f3c35a2ee
Josefin Fundin <josefin@fundin.com>
2020-01-17 11:58:05 +0000

    updated id function and moved it outsde of add item function. added query to get all tags for a specific item id and a seperate function that collates it to a list. Added test for tag functions

--- database_manager.py
+++ database_manager.py
@@ -1,36 +1,48 @@
 import database_helper
 
+
 class ItemManager:
 
     def __init__(self, database_connector):
         self.database_connector = database_connector
-    
-    def add_item(self):
-        item_id = database_helper.get_uuid()
-        self.database_connector.execute("INSERT INTO Items (ItemId, VectorPath, PngPath) VALUES (%s, %s, %s)", (item_id, item_id+"_vector", item_id+"_png"))
-        return item_id
+
+    def add_item(self, item_id):
+        self.database_connector.execute(
+            "INSERT INTO items (item_id, vector_path, png_path) VALUES (%s, %s, %s)", (item_id, item_id+"_vector", item_id+"_png"))
 
     def delete_item(self, item_id):
-        self.database_connector.execute("DELETE FROM Items WHERE ItemId=%s", (item_id))
-    
-    def get_item(self, item_id):
-        item = self.database_connector.execute("SELECT vector_path, png_path from items WHERE item_id=%s", (item_id))
-        return item
-    
+        self.database_connector.execute(
+            "DELETE FROM Items WHERE ItemId=%s", (item_id))
+
+    def get_item_details(self, item_id):
+        item_details = self.database_connector.execute(
+            "SELECT * from items WHERE item_id=%s", (item_id))
+        return item_details[0]
+
 
 class TagManager:
 
     def __init__(self, database_connector):
         self.database_connector = database_connector
-    
+
     def add_tag(self, tag_name, item_id):
-        tag_id = database_helper.get_uuid()
-        self.database_connector.execute("INSERT INTO Tags (TagId, Tag, ItemId) VALUES (%s, %s, %s)", (tag_id, tag_name, item_id))
-    
+        tag_id = database_helper.get_id()
+        self.database_connector.execute(
+            "INSERT INTO Tags (TagId, Tag, ItemId) VALUES (%s, %s, %s)", (tag_id, tag_name, item_id))
+
     def get_all_matches(self, search_word):
-        all_matches = self.database_connector.execute("SELECT * FROM tags WHERE tag LIKE %s;", ('%'+search_word+'%'))
+        all_matches = self.database_connector.execute(
+            "SELECT * FROM tags WHERE tag LIKE %s;", ('%'+search_word+'%'))
         return all_matches
 
+    def get_tags_list(self, item_id):
+        item_tags = self.database_connector.execute(
+            "SELECT tag FROM tags WHERE item_id=%s", (item_id))
+        tags_list = self._collate_tags_to_list(item_tags)
+        return tags_list
 
-    
-
+    def _collate_tags_to_list(self, item_tags):
+        tags_list = []
+        for tag in item_tags:
+            tags_list.append(tag['tag'])
+        return tags_list
