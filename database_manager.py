516bd9e7046c9346de6aff338e37b98989be7917
Josefin Fundin <josefin@fundin.com>
2020-01-21 17:28:05 +0000

    formatting fixes, added docstrings and mimised some functions suggested in review

--- database_manager.py
+++ database_manager.py
@@ -8,17 +8,20 @@ class ItemManager:
 
     def add_item(self, item_id):
         self.database_connector.execute(
-            "INSERT INTO items (ItemId, VectorPath, PngPath) VALUES (%s, %s, %s)", (item_id, item_id+"_vector", item_id+"_png"))
+            "INSERT INTO items (ItemId, VectorPath, PngPath) VALUES (%s, %s, %s)",
+            (item_id, item_id+"_vector", item_id+"_png"))
 
     def delete_item(self, item_id):
         self.database_connector.execute(
-            "DELETE FROM Items WHERE ItemId=%s", (item_id))
+            "DELETE FROM Items WHERE ItemId=%s", item_id)
 
     def get_item_details(self, item_id):
         item_details = self.database_connector.execute(
-            "SELECT * from Items WHERE ItemId=%s", (item_id))
+            "SELECT * from Items WHERE ItemId=%s", item_id)
+        assert len(item_details) == 1
         return item_details[0]
-    
+
+
 class TagManager:
 
     def __init__(self, database_connector):
@@ -27,31 +30,29 @@ class TagManager:
     def add_tag(self, tag_name, item_id):
         tag_id = database_helper.get_id()
         self.database_connector.execute(
-            "INSERT INTO Tags (TagId, Tag, ItemId) VALUES (%s, %s, %s)", (tag_id, tag_name, item_id))
+            "INSERT INTO Tags (TagId, Tag, ItemId) VALUES (%s, %s, %s)",
+            (tag_id, tag_name, item_id))
 
     def get_all_matches(self, search_word):
         all_matches = self.database_connector.execute(
             "SELECT * FROM Tags WHERE Tag LIKE %s;", ('%'+search_word+'%'))
         return all_matches
-    
+
     def get_item_tags_list(self, item_id):
-        item_tags = self.database_connector.execute("SELECT Tag FROM Tags WHERE ItemId=%s", (item_id))
+        item_tags = self.database_connector.execute(
+            "SELECT Tag FROM Tags WHERE ItemId=%s", item_id)
         tags_list = self._collate_tags_to_list(item_tags)
         return tags_list
 
     def _collate_tags_to_list(self, item_tags):
-        tags_list = []
-        for tag in item_tags:
-            tags_list.append(tag['Tag'])
-        return tags_list
-    
+        return list(tag['Tag'] for tag in item_tags)
+
     def get_unique_tags_list(self):
-        unique_tags = self.database_connector.execute("SELECT Tag, count(*) as count FROM Tags GROUP By Tag;")
-        return unique_tags
-    
+        """Return: tuple with tag name and their count of occurances."""
+        unique_tags = self.database_connector.execute(
+            "SELECT Tag, count(*) as count FROM Tags GROUP By Tag;")
+        return tuple((tag['Tag'], tag['count']) for tag in unique_tags)
+
     def delete_tags_for_item(self, item_id):
-        self.database_connector.execute("DELETE FROM Tags WHERE ItemId=%s", (item_id))
-
-
-    
-
+        self.database_connector.execute(
+            "DELETE FROM Tags WHERE ItemId=%s", item_id)
