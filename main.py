35884235101e0a52c062a08124ddfd843ca51eaf
Josefin Fundin <josefin@fundin.com>
2020-01-21 17:29:25 +0000

    fixed typo in funktion and cleared out unessicary elses in APIs

--- main.py
+++ main.py
@@ -25,40 +25,32 @@ def index():
 def unique_tags():
     if request.method == 'GET':
         return jsonify(tag_manager.get_unique_tags_list())
-    else:
-        return "no tags requested"
 
 
 @app.route('/add_product', methods=['GET', 'POST'])
 def add_product():
-    if request.method == 'GET':
+    if request.method == 'POST':
         # TODO: updated tags below to get data from FE
         tags = ["bird", "hello"]
         product_manager.add_product(tags)
         return 'A product has been added'
-    else:
-        return "no button clicked yet to add a product"
 
 
 @app.route('/delete_product', methods=['GET', 'POST'])
 def delete_product():
-    if request.method == 'GET':
+    if request.method == '`POST`':
         # TODO: update POST and to recieve item_id from FE
         item_id = "6baa81a4-5a9d-4130-bc5f-1e842d1ecb53"
         product_manager.delete_product(item_id)
         return "A product has been deleted"
-    else:
-        return "no button clicked yet to delete a product"
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     # TODO: hook up actual request argument below ones FE is a built and update to POST
     search_word = "sw"
-    if request.method == 'GET':
-        return jsonify(search_manager.get_all_maching_products(search_word))
-    else:
-        return "no word submitted yet"
+    if request.method == 'POST':
+        return jsonify(search_manager.get_all_matching_products(search_word))
 
 
 if __name__ == '__main__':
--- search_manager.py
+++ search_manager.py
@@ -3,7 +3,7 @@ class SearchManager:
         self.tag_manager = tag_manager
         self.item_manager = item_manager
     
-    def get_all_maching_products(self, search_word):
+    def get_all_matching_products(self, search_word):
         all_maching_products = []
         all_tag_maches = self.tag_manager.get_all_matches(search_word)
         for match in all_tag_maches:
@@ -13,6 +13,3 @@ class SearchManager:
             item_details.update({'Tags' : item_tag_list})
             all_maching_products.append(item_details)
         return all_maching_products
-
-
-
