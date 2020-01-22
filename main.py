eae7ac93f00f93cc4d311b167a9ae3b7c3bd3b01
Josefin Fundin <josefin@fundin.com>
2020-01-22 17:17:25 +0000

    cleaned up APIs so match a RESTful api structure

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from flask import Flask, jsonify
 
 from database_connector import DatabaseConnector
 from database_manager import ItemManager, TagManager
-from search_manager import SearchManager
 from product_manager import ProductManager
 
 database_connector = DatabaseConnector('db')
 connection = database_connector.get_connection()
 item_manager = ItemManager(database_connector)
 tag_manager = TagManager(database_connector)
-search_manager = SearchManager(tag_manager, item_manager)
 product_manager = ProductManager(item_manager, tag_manager)
 
 app = Flask(__name__)
@@ -21,19 +19,28 @@ def index():
     return 'Hello, World, this is the index!'
 
 
-@app.route('/unique_tags', methods=['GET', 'POST'])
-def unique_tags():
+@app.route('/tags', methods=['GET'])
+def tags():
     if request.method == 'GET':
         return jsonify(tag_manager.get_unique_tags_list())
 
 
-@app.route('/add_product', methods=['GET', 'POST'])
-def add_product():
-    if request.method == 'POST':
+@app.route('/search', methods=['GET'])
+def search():
+    search_word = "sw"  # TODO: hook up to actual input
+    if request.method == "GET":
+        return jsonify(product_manager.get_all_matching_products(search_word))
+
+
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    if request.method == "GET":
+        # TODO: Get all items with assosiated tags, just like with search words but all items
+        return None
+    elif request.method == 'POST':
         # TODO: updated tags below to get data from FE
         tags = ["bird", "hello"]
         product_manager.add_product(tags)
-        return 'A product has been added'
 
 @app.route('/delete_product', methods=['GET', 'POST'])
 def delete_product():
@@ -45,21 +52,17 @@ def delete_product():
         return "no button clicked yet to delete a product"
 
 
-@app.route('/delete_product', methods=['GET', 'POST'])
-def delete_product():
-    if request.method == '`POST`':
-        # TODO: update POST and to recieve item_id from FE
-        item_id = "6baa81a4-5a9d-4130-bc5f-1e842d1ecb53"
+@app.route('/product/<string:item_id>', methods=['GET', 'PUT', 'DELETE'])
+def product(item_id):
+    if request.method == "GET":
+        # TODO: Get item with matching id
+        return None
+    elif request.method == 'PUT':
+        # TODO: update/edit product
+        return None
+    elif request.method == 'DELETE':
         product_manager.delete_product(item_id)
         return "A product has been deleted"
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    # TODO: hook up actual request argument below ones FE is a built and update to POST
-    search_word = "sw"
-    if request.method == 'POST':
-        return jsonify(search_manager.get_all_matching_products(search_word))
 
 
 if __name__ == '__main__':
