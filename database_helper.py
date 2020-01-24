19d7757f1a546af004039305287c3f3ff61af1a8
Josefin Fundin <josefin@fundin.com>
2020-01-24 12:17:01 +0000

    added function to check if an id is valid

--- database_helper.py
+++ database_helper.py
@@ -1,4 +1,11 @@
 import uuid
 
+
 def get_id():
-    return str(uuid.uuid4())  
+    return str(uuid.uuid4())
+
+
+def is_valid_id(id):
+    if str(uuid.UUID(id)) == id:
+        return True
+    return False
