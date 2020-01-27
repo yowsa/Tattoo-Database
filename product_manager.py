fecd99a7f665b6834287a1a8d4801e29627a82ad
Josefin Fundin <josefin@fundin.com>
2020-01-27 13:03:06 +0000

    added missing tests for product manager ang updated for it to work with mock aws server

--- product_manager.py
+++ product_manager.py
@@ -8,27 +8,28 @@ class ProductManager:
         self.tag_manager = tag_manager
         self.image_manager = image_manager
 
-    def add_product(self, tags, vector, png):
+    def add_product(self, tags, vector_file, png_file):
         item_id = helper.get_id()
-        vector_path = self.image_manager.add_image(vector, item_id, True)
-        png_path = self.image_manager.add_image(png, item_id, False)
+        vector_path = self.image_manager.add_image(vector_file, item_id, True)
+        png_path = self.image_manager.add_image(png_file, item_id, False)
         self.item_manager.add_item(item_id, vector_path, png_path)
         for tag in tags:
             self.tag_manager.add_tag(tag, item_id)
+        return item_id
 
     def delete_product(self, item_id):
         self.tag_manager.delete_tags_for_item(item_id)
         self.item_manager.delete_item(item_id)
         self.image_manager.delete_image(item_id)
-    
+
     def get_all_products(self):
         all_items = self.item_manager.get_all_items()
         for item in all_items:
             item_id = item["ItemId"]
             item_tag_list = self.tag_manager.get_item_tags(item_id)
-            item.update({'Tags' : item_tag_list})
+            item.update({'Tags': item_tag_list})
         return all_items
-    
+
     def get_all_matching_products(self, search_word):
         all_maching_products = []
         all_tag_maches = self.tag_manager.get_all_matches(search_word)
@@ -36,7 +37,6 @@ class ProductManager:
             item_id = match["ItemId"]
             item_tag_list = self.tag_manager.get_item_tags(item_id)
             item_details = self.item_manager.get_item_details(item_id)
-            item_details.update({'Tags' : item_tag_list})
+            item_details.update({'Tags': item_tag_list})
             all_maching_products.append(item_details)
         return all_maching_products
-
--- tests/test_product_manager.py
+++ tests/test_product_manager.py
@@ -1,14 +1,18 @@
 
 from . import setup_test
 import unittest
+from moto import mock_s3
 import database_connector
 import database_manager
 import product_manager
-import helper
-import image_manager
+from image_manager import ImageManager
+from image_manager import AWSConnector
 
 
-class TestDatabaseManager(unittest.TestCase):
+@mock_s3
+class TestProductManager(unittest.TestCase):
+    bucket = 'jf-test555-bucket'
+
     @classmethod
     def setUpClass(self):
         self.database_connector = database_connector.DatabaseConnector()
@@ -16,12 +20,57 @@ class TestDatabaseManager(unittest.TestCase):
             self.database_connector)
         self.tag_manager = database_manager.TagManager(
             self.database_connector)
-        self.image_manager = image_manager.ImageManager()
+        aws_connector = AWSConnector()
+        self.s3_resource = aws_connector.get_s3_resource()
+        self.image_manager = ImageManager(self.s3_resource, self.bucket)
         self.product_manager = product_manager.ProductManager(
             self.item_manager, self.tag_manager, self.image_manager)
 
     def setUp(self):
         setup_test.create_test_database_setup(self.database_connector)
+        self.s3_resource.create_bucket(Bucket=self.bucket, CreateBucketConfiguration={
+            'LocationConstraint': 'eu-west-2'},)
+
+    def test_add_product(self):
+        # arrange
+        tags = ['bird', 'fineline']
+        vector_file = "tests/test2.jpg"
+        png_file = "tests/test2.jpg"
+
+        # act
+        self.product_manager.add_product(tags, vector_file, png_file)
+
+        # assert
+        self.assertTrue(setup_test.assertCount(
+            self.database_connector, 'Items', 1))
+        self.assertTrue(setup_test.assertCount(
+            self.database_connector, 'Tags', 2))
+
+    def test_delete_product(self):
+        # arrange
+        tags = ['bird', 'fineline']
+        vector_file = "tests/test2.jpg"
+        png_file = "tests/test2.jpg"
+        item_id = self.product_manager.add_product(tags, vector_file, png_file)
+
+        # act
+        self.product_manager.delete_product(item_id)
+
+        # assert
+        self.assertTrue(setup_test.assertCount(
+            self.database_connector, 'Items', 0))
+        self.assertTrue(setup_test.assertCount(
+            self.database_connector, 'Tags', 0))
+
+    def test_get_all_products(self):
+        # arrange
+        setup_test.test_items_tags_setup(self.item_manager, self.tag_manager)
+
+        # act
+        all_products = self.product_manager.get_all_products()
+
+        # assert
+        self.assertEqual(len(all_products), 4)
 
     def test_get_all_matching_products(self):
         # arrange
@@ -37,6 +86,10 @@ class TestDatabaseManager(unittest.TestCase):
 
     def tearDown(self):
         setup_test.tear_down_database_setup(self.database_connector)
+        bucket = self.s3_resource.Bucket(self.bucket)
+        for key in bucket.objects.all():
+            key.delete()
+        bucket.delete()
 
     @classmethod
     def tearDownClass(self):
