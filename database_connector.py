893009ccf2dc5952b4633a5d50e185dba5ea9d85
Josefin Fundin <josefin@fundin.com>
2020-02-07 16:05:33 +0000

    added response enum and impemented it in the database connection function

--- database_connector.py
+++ database_connector.py
@@ -1,11 +1,15 @@
 import pymysql.cursors
 from config import DatabaseConf
+from response import Response
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class DatabaseConnector:
     charset = 'utf8mb4'
     cursorclass = pymysql.cursors.DictCursor
-    
+
     def __init__(self, database=None, host="localhost", user="root", password=""):
         self.db = database
         self.host = host
@@ -16,17 +20,21 @@ class DatabaseConnector:
         self.db = database
 
     def get_connection(self):
-        return pymysql.connect(host=self.host,
-                                     user=self.user,
-                                     password=self.password,
-                                     db=self.db,
-                                     charset=self.charset,
-                                     cursorclass=self.cursorclass)
+        try:
+            return pymysql.connect(host=self.host,
+                                   user=self.user,
+                                   password=self.password,
+                                   db=self.db,
+                                   charset=self.charset,
+                                   cursorclass=self.cursorclass)
+        except pymysql.err.OperationalError:
+            logger.error('Wrong Database credentials', exc_info=True)
+            return Response.NOT_CONNECTED.message("Not connected to database due to wrong credentials.")
     
     def create_database(self, database):
         query = "CREATE DATABASE IF NOT EXISTS " + database
         self.execute(query)
-    
+
     def create_tables(self):
         self.execute(DatabaseConf.ITEM_TABLE_SCHEMA)
         self.execute(DatabaseConf.TAGS_TABLE_SCHEMA)
---
+++ response.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Response(Enum):
+    OK = 1
+    USER_ERROR = 2
+    SERVER_ERROR = 3
+    INVALID_FILE = 4
+    INVALID_FORMAT = 5
+    FILE_NOT_FOUND = 6
+    NOT_CONNECTED = 7
+    CONNECTED = 8
+    UNKNOWN_ERROR = 9
+
+    def message(self, message: str, body=None):
+        return {'ErrorCode': self.name, 'Message': message, 'Body': body}
